77c84505319a72fce31c751d33577a81bb77e7d1
Gideon Tong <gideon@gideontong.com>
2021-01-27 14:55:13 -0800

    Create window

--- interface/__init__.py
+++ interface/__init__.py
@@ -1,16 +1,40 @@
-from PySide6.QtWidgets import QApplication, QPushButton, QVBoxLayout, QWidget
-from PySide6.QtGui import QIcon
+from PySide6.QtWidgets import (QApplication,
+                               QLabel,
+                               QPushButton,
+                               QVBoxLayout,
+                               QFileDialog,
+                               QWidget)
+from PySide6.QtGui import (QIcon,
+                           QPixmap)
+from interface.process import process_ics
 
 class DCCUI(QWidget):
     def __init__(self):
         super().__init__()
-        self.ics_selector = QPushButton('Select ICS file')
-        self.layout = QVBoxLayout()
-        self.layout.addWidget(self.ics_selector)
         self.setWindowTitle('DCC by Gideon Tong')
         self.icon = QIcon('icons/icon.png')
         self.setWindowIcon(self.icon)
-        self.setLayout(self.layout)
+        self.setMainWindow()
+
+    def setMainWindow(self):
+        layout = QVBoxLayout()
+        image_map = QPixmap('icons/cat.jpg')
+        image_label = QLabel(self)
+        image_label.setPixmap(image_map)
+        ics_selector = QPushButton('Select iCalendar (Google Calendar, Apple Calendar)')
+        ics_selector.clicked.connect(self.selectFile)
+        layout.addWidget(image_label)
+        layout.addWidget(ics_selector)
+        self.setLayout(layout)
+
+    def selectFile(self):
+        filename = QFileDialog.getOpenFileName(self,
+                                               "Open Calendar File",
+                                               "",
+                                               "iCalendar (*.ics)")
+        if filename and filename[0]:
+            process_ics(filename[0])
+
 
 def generate_app() -> tuple:
     return QApplication([])
---
+++ interface/process.py
@@ -0,0 +1,19 @@
+from ics import Calendar
+
+def add_to_data(data: dict, time: 'Arrow', name: str) -> None:
+    if time.year not in data:
+        data[time.year] = dict()
+    if time.month not in data[time.year]:
+        data[time.year][time.month] = dict()
+    if time.day not in data[time.year][time.month]:
+        data[time.year][time.month][time.day] = list()
+    data[time.year][time.month][time.day].append(name)
+
+def process_ics(file: str) -> dict:
+    data = dict()
+    with open(file, encoding='utf-8') as ics_file:
+        calendar = Calendar(ics_file.read())
+        for event in calendar.events:
+            time = event.begin.to('Asia/Taipei')
+            add_to_data(data, time, event.name)
+    return data
